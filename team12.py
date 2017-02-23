28345c513492a820d915e12eba48a884faa6314d
tiyousdallas <039700@daltonk12.onmicrosoft.com>
2017-02-23 14:24:47 -0500

    team12.py
    Updated strategy

--- team12.py
+++ team12.py
@@ -1,39 +1,14 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'ArmyOfTwo' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-  
+team_name = 'ArmyOfTwo'
+strategy_name = 'Betray Only'
+strategy_description = 'Only return B'
+    
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+ 
+    return 'bccb'
 
     
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
+def tit_tat(my_history, their_history, my_score, their_score, result):
+  
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
@@ -47,22 +22,19 @@ def test_move(my_history, their_history, my_score, their_score, result):
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+    if tit_tat(my_history='b',
+              their_history='c', 
+              my_score=100,
+              their_score=-500,
+              result='bccb'):
+         print 'Move working.'
+    tit_tat(my_history='bc',
+              their_history='cc', 
+              my_score=100, 
+              their_score=-500,
+              result='bccb')             
+    tit_tat(my_history='bbb',
+              their_history='cbc',
+              my_score=200,
+              their_score=-1000,
+              result='bccb')
